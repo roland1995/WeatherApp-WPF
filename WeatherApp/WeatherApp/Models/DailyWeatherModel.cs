4912ec3957221136bc1d27cf67e2a96b2136e16a
Roland Horváth <rolandhorvath1025@gmail.com>
2021-05-16 00:06:15 +0200

    Fill up DailyWeatherModel

--- WeatherApp/WeatherApp/Models/DailyWeatherModel.cs
+++ WeatherApp/WeatherApp/Models/DailyWeatherModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace WeatherApp.Models
 {
     class DailyWeatherModel
     {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("weather")]
+        public IList<IDictionary<string, string>> Weather { get; set; }
+
+        [JsonPropertyName("main")]
+        public IDictionary<string, string> Main { get; set; }
     }
+}
 }
