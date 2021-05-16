f323bb127f35fbe19c9ba4a74b17ba7a823448ff
Roland Horváth <rolandhorvath1025@gmail.com>
2021-05-16 02:56:04 +0200

    Setup WeeklyWeatherModel

---
+++ WeatherApp/WeatherApp/Models/WeeklyWeatherModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace WeatherApp.Models
+{
+    class WeeklyWeatherModel
+    {
+        [JsonPropertyName("weather")]
+        public IList<IDictionary<string, string>> Weather { get; set; }
+
+        [JsonPropertyName("dt_txt")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("main")]
+        public IDictionary<string, string> Main { get; set; }
+    }
+}
