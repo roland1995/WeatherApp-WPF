c761c1569c799e5d72a7456b09aef39614941af4
Roland Horváth <rolandhorvath1025@gmail.com>
2021-05-16 02:07:15 +0200

    Create Polyline points in WeeklyViewModel
    Use in Weekly view with Binding

--- WeatherApp/WeatherApp/ViewModels/WeeklyViewModel.cs
+++ WeatherApp/WeatherApp/ViewModels/WeeklyViewModel.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows;
 using System.Text;
+using System.Windows.Media;
 
 namespace WeatherApp.ViewModels
 {
     public class WeeklyViewModel
     {
+        private short[] dataXCoordinate = new short[] { 10, 150, 300, 450, 600 };
+        private short[] dataMax1 = new short[] { 400, 330, 301, 315, 312 }; 
+        private short[] dataMin = new short[] { 10, 150, 300, 450, 600 };
+        private short[] dataMin1 = new short[] { 400, 350, 340, 345, 350 };
+
+
+    public WeeklyViewModel()
+        {
+            Setup();
+      
+        }
+
+        private void Setup()
+        {
+            FillUpPolyLines();
+        }
+
+        private void FillUpPolyLines()
+        {
+            for (int i = 0; i < dataXCoordinate.Length; i++)
+            {
+                PointsMax.Add(new Point(dataXCoordinate[i], dataMax1[i]));
+                PointsMin.Add(new Point(dataXCoordinate[i], dataMin1[i]));
+            }
+        }
+
+        private PointCollection _pointsMax = new PointCollection();
+        public PointCollection PointsMax
+        {
+            get { return _pointsMax; }
+        }
+        private PointCollection _pointsMin = new PointCollection();
+        public PointCollection PointsMin
+        {
+            get { return _pointsMin; }
+        }
     }
 }
